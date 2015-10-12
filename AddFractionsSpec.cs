9216071c347430b3e1576bc5bc5e7b5984fe2ac3
Peter Gfader <pgf@zuehlke.com>
2015-10-12 15:41:52 +0200

    added KentBeck test and empty folder test

--- AddFractionsSpec.cs
+++ AddFractionsSpec.cs
@@ -10,5 +10,13 @@ namespace Specs
             Fraction sum = new Fraction(0).Plus(new Fraction(0));
             Assert.Equal(0, sum.IntegerValue);
         }
+
+        // Kent Beck would write 2 PLUS 5 as 1st test
+        //[Fact]
+        //public void TwoPlusTen()
+        //{
+        //    Fraction sum = new Fraction(2).Plus(new Fraction(5));
+        //    Assert.Equal(10, sum.IntegerValue);
+        //}
     }
 }
