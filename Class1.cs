b797ef8778c3208fa818ba30517add2c092c6617
Peter Gfader <pgf@zuehlke.com>
2015-10-12 15:27:48 +0200

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specs
+{
+    using Xunit;
+
+    public class Class1
+    {
+
+        [Fact]
+        public void Should_do_When_doing()
+        {
+            Assert.True(true);
+        }
+    }
+}
