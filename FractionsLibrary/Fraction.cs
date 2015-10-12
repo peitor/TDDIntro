85b3d22d2967b27d108a38266482a2ac9f00e354
Peter Gfader <pgf@zuehlke.com>
2015-10-12 23:56:07 +0200

    Moving files around

---
+++ FractionsLibrary/Fraction.cs
@@ -0,0 +1,18 @@
+namespace FractionsLibrary
+{
+    public class Fraction
+    {
+        public Fraction(int n, int d)
+        {
+            
+
+        }
+
+        public double Nominator { get; set; }
+
+        public Fraction Add(Fraction fraction)
+        {
+            return new Fraction(0,0);
+        }
+    }
+}
