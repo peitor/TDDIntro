ec58f5f88e7bbc843f70056230cecc92fde10f8d
Peter Gfader <pgf@zuehlke.com>
2015-10-12 15:42:59 +0200

    empty fodler not committed???ß

---
+++ test-empty-folder/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specs.test_empty_folder
+{
+    class Class1
+    {
+    }
+}
